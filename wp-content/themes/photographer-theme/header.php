e015bb2b8923ec46ac20c1d1dc0a6e5145d455dc
Emroze <emrozesd@users.noreply.github.com>
2019-09-13 18:26:10 -0700

    phpcs wp functioning! Finally,

--- wp-content/themes/photographer-theme/header.php
+++ wp-content/themes/photographer-theme/header.php
@@ -16,14 +16,12 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+    
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'photographer-theme' ); ?></a>
-
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
