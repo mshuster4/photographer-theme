25dd739e9cc2c606a8d19536dfe69682101dbdf0
Emroze <emrozesd@users.noreply.github.com>
2019-09-16 21:01:39 -0700

    added bootstrap

--- wp-content/themes/photographer-theme/functions.php
+++ wp-content/themes/photographer-theme/functions.php
@@ -159,3 +159,16 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+
+function my_scripts_enqueue() {
+
+	wp_register_script( 'popper-js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array('jquery'), NULL, true );
+	wp_register_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', array('jquery'), NULL, true );
+	wp_register_style( 'bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css', false, NULL, 'all' );
+	
+	wp_enqueue_script( 'popper-js' );
+	wp_enqueue_script( 'bootstrap-js' );
+	
+	wp_enqueue_style( 'bootstrap-css' );
+	}
+	add_action( 'wp_enqueue_scripts', 'my_scripts_enqueue' );
--- wp-content/themes/photographer-theme/index.php
+++ wp-content/themes/photographer-theme/index.php
@@ -3,13 +3,15 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-	  <main id="main" class="site-main">
-		<h1>Hello World</h1>
-      </main>
+<div id="primary" class="content-area">
+  <main id="main" class="site-main">
+    <div class="container">
+     <h1>Hello World</h1>
     </div>
+  </main>
+</div>
 
 <?php
-	get_sidebar();
-	get_footer();
+get_sidebar();
+get_footer();
 ?>
